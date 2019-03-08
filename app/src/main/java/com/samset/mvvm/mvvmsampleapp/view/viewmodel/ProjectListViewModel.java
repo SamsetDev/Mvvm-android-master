e50ce22ecf5f9fbd782e90c87b4cae9fadf8b00d
SamsetDev <sanjay.singh@glasswing.in>
2019-03-08 15:40:06 +0530

    add error handling

--- app/src/main/java/com/samset/mvvm/mvvmsampleapp/view/viewmodel/ProjectListViewModel.java
+++ app/src/main/java/com/samset/mvvm/mvvmsampleapp/view/viewmodel/ProjectListViewModel.java
@@ -2,34 +2,32 @@ package com.samset.mvvm.mvvmsampleapp.view.viewmodel;
 
 import android.app.Application;
 
-import androidx.lifecycle.AndroidViewModel;
-import androidx.lifecycle.LiveData;
-import androidx.annotation.NonNull;
-import androidx.lifecycle.MutableLiveData;
-
-import com.samset.mvvm.mvvmsampleapp.remote.service.model.Project;
+import com.samset.mvvm.mvvmsampleapp.listeners.NetworkResponse;
+import com.samset.mvvm.mvvmsampleapp.remote.service.model.ProjectResponse;
 import com.samset.mvvm.mvvmsampleapp.remote.service.repository.ProjectRepository;
-
-import java.util.List;
 
 import javax.inject.Inject;
 
+import androidx.annotation.NonNull;
+import androidx.lifecycle.LiveData;
+
 public class ProjectListViewModel extends BaseViewModel {
 
-    private final LiveData<List<Project>> projectListObservable;
+    private final LiveData<ProjectResponse> projectListObservable;
+    private NetworkResponse response=null;
 
     @Inject
     public ProjectListViewModel(@NonNull ProjectRepository projectRepository, @NonNull Application application) {
         super(projectRepository, application);
         // If any transformation is needed, this can be simply done by Transformations class ...
-        projectListObservable = projectRepository.getProjectList("Google");
+        projectListObservable = projectRepository.getProjectList("Google",response);
 
     }
 
     /**
      * Expose the LiveData Projects query so the UI can observe it.
      */
-    public LiveData<List<Project>> getProjectListObservable() {
+    public LiveData<ProjectResponse> getProjectListObservable() {
         return projectListObservable;
     }
 
